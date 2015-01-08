79018a0e5ed170077c205c0af05c12c00a7ba6a2
Luuk Dominiek Jansen OP <luukajansen@gmail.com>
2015-01-08 08:47:26 +0000

    - small changes

--- app/controllers/Devices.java
+++ app/controllers/Devices.java
@@ -1,11 +1,16 @@
 package controllers;
 
 import models.Device;
+import models.DeviceRole;
 import play.data.DynamicForm;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
+import scala.collection.Seq;
+import scala.collection.convert.Wrappers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -34,7 +39,12 @@ public class Devices extends Controller {
         Form<Device> myForm = Form.form(Device.class);
         myForm = myForm.fill(device);
 
-        return ok(views.html.Devices.edit.render(myForm));
+        List<String> list = new ArrayList<String>();
+        for(DeviceRole role : DeviceRole.values()) {
+            list.add(role.toString());
+        }
+
+        return ok(views.html.Devices.edit.render(myForm, list));
     }
 
     public static Result save () {
